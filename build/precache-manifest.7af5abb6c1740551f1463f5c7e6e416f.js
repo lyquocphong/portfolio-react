4636d9e2209c1cd84006654e5f8c792fe767663d
Ly Quoc Phong <lyquocphong@gmail.com>
2019-01-07 23:37:53 +0200

    Commit prodúction landing page

---
+++ build/precache-manifest.7af5abb6c1740551f1463f5c7e6e416f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec8abebb70a8279e9636f4d552910700",
+    "url": "/static/media/phonglee.ec8abebb.png"
+  },
+  {
+    "revision": "f6ab04aed30a8643bf94fe00f7ff0b59",
+    "url": "/static/media/icofont.f6ab04ae.woff"
+  },
+  {
+    "revision": "50a4ab76e700a83e649be213f820fbbd",
+    "url": "/static/media/icofont.50a4ab76.woff2"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7cf3eadf58063fe530c6",
+    "url": "/static/js/main.7cf3eadf.chunk.js"
+  },
+  {
+    "revision": "ac9ae1edcdde807d9986",
+    "url": "/static/js/1.ac9ae1ed.chunk.js"
+  },
+  {
+    "revision": "7cf3eadf58063fe530c6",
+    "url": "/static/css/main.d5ff25b3.chunk.css"
+  },
+  {
+    "revision": "67a999577b14d57f310546d1ecd9f481",
+    "url": "/index.html"
+  }
+];
